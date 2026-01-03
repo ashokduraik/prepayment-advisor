a4b5a9c6acf10fefc6a2d9f1b4efdac5f23657f7
Ashok Durai K <ashokduraik@gmail.com>
2026-01-03 09:40:09 +0300

    chore: register Ionic custom elements at startup (defineCustomElements)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// Ensure Ionic web components are registered when running in the browser
+import { defineCustomElements } from '@ionic/core/loader';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+
+// Register Ionic custom elements
+defineCustomElements(window as any);
 
 platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.log(err));
